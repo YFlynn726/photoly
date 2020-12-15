0f2719ad14c81fefc93e8570c613fca295b16e73
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-12-15 00:08:08 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,67 @@
+const auth = "563492ad6f91700001000001e6f2bdf11dc94081b1dea6e1b31a754d";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const form = document.querySelector(".search-form");
+let searchValue;
+
+//event listerner
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
+  const data = await dataFetch.json();
+  return data;
+}
+
+function generatePhotos(data) {
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `
+        <div class="gallery-info">
+        <p>${photo.photographer}</p>
+        <a href=${photo.src.original}>Download</a>
+        </div>
+        <img src=${photo.src.large}></img>
+        `;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function curatedPhotos() {
+  const data = await fetchApi(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1"
+  );
+  //console.log(data);
+  generatePhotos(data);
+}
+
+async function searchPhotos(query) {
+  clear();
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
+  );
+  //console.log(data);
+
+  generatePhotos(data);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
+}
+curatedPhotos();
